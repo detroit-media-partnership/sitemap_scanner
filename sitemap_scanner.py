422f094039b7484939790037ebcdc512349bb4ed
Eric Bower <neurosnap@gmail.com>
2014-08-22 15:03:55 -0400

    removed external link check

--- sitemap_scanner.py
+++ sitemap_scanner.py
@@ -14,8 +14,6 @@ def get_links(url):
             continue
         if curr_link in scanned_links:
             continue
-        if in_external_links(curr_link):
-            continue
         if is_blacklisted(curr_link):
             continue
 
