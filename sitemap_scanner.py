3e0fb5d35e235574ea41cef49dd16d634e2582b2
Eric Bower <neurosnap@gmail.com>
2014-08-22 14:14:27 -0400

    updated logic in get_links

--- sitemap_scanner.py
+++ sitemap_scanner.py
@@ -10,24 +10,28 @@ def get_links(url):
     for link in links.find():
         curr_link = link['href']
 
-        continue_flag = False
-        for item in blacklist:
-            if re.search(item, curr_link):
-                continue_flag = True
-                break
-        if continue_flag:
+        if curr_link == url:
+            continue
+        if curr_link in scanned_links:
+            continue
+        if curr_link in external_links:
+            continue
+        if is_blacklisted(curr_link):
             continue
 
-        if (url != curr_link 
-            and curr_link not in scanned_links 
-            and re.search(domain, curr_link)):
-                print "Scanning page: %s" % curr_link
-                scanned_links.append(curr_link)
-                get_links(curr_link)
-        elif (not re.search(domain, curr_link) 
-            and curr_link not in external_links):
-                print "Found: %s" % curr_link
-                external_links.append(curr_link)
+        if re.search(domain, curr_link):
+            print "Scanning page: %s" % curr_link
+            scanned_links.append(curr_link)
+            get_links(curr_link)
+        else:
+            print "Found: %s" % curr_link
+            external_links.append(curr_link)
+
+def is_blacklisted(url):
+    for item in blacklist:
+        if re.search(item, url):
+            return True
+    return False
 
 if __name__ == '__main__':
     if not sys.argv[1]:
@@ -48,9 +52,9 @@ if __name__ == '__main__':
             fp.write("Scanned\n--------------------\n")
             for link in scanned_links:
                 fp.write(link + "\n")
-            fp.write("Found\n--------------------\n")
+            fp.write("\nFound\n--------------------\n")
             for link in external_links:
                 fp.write(link + "\n")
-            fp.write("--------------------\n")
+            fp.write("\n--------------------\n")
             fp.write("Total pages scanned: %s\n" % len(scanned_links))
             fp.write("Total external links found: %s" % len(external_links))
