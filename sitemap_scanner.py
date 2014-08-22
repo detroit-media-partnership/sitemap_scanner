a05007d615254a70cedfa004ae81463748523904
Eric Bower <neurosnap@gmail.com>
2014-08-22 14:53:54 -0400

    added text and where the found link came from

--- sitemap_scanner.py
+++ sitemap_scanner.py
@@ -8,12 +8,13 @@ def get_links(url):
     links = linkGrabber.Links(url)
     for link in links.find():
         curr_link = link['href']
+        curr_text = link['text'].encode('ascii', 'ignore').decode('ascii')
 
         if curr_link == url:
             continue
         if curr_link in scanned_links:
             continue
-        if curr_link in external_links:
+        if in_external_links(curr_link):
             continue
         if is_blacklisted(curr_link):
             continue
@@ -23,8 +24,14 @@ def get_links(url):
             scanned_links.append(curr_link)
             get_links(curr_link)
         else:
-            print "Found: %s" % curr_link
-            external_links.append(curr_link)
+            print "Found: %s <%s> from %s" % (curr_text, curr_link, url)
+            external_links.append({ "link": curr_link, "text": curr_text, "from": url })
+
+def in_external_links(url):
+    for link in external_links:
+        if url == link['link']:
+            return True
+    return False
 
 def is_blacklisted(url):
     for item in blacklist:
@@ -53,7 +60,7 @@ if __name__ == '__main__':
                 fp.write(link + "\n")
             fp.write("\nFound\n--------------------\n")
             for link in external_links:
-                fp.write(link + "\n")
+                fp.write("%s <%s> (from %s)\n" % (link['text'], link['link'], link['from']))
             fp.write("\n--------------------\n")
             fp.write("Total pages scanned: %s\n" % len(scanned_links))
             fp.write("Total external links found: %s" % len(external_links))
